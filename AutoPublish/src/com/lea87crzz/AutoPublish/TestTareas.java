02d77fe366a7f07b379e56d106a135a5e59560f7
lpujol <lpujol@15-021.conicet.gov.ar>
2017-06-16 17:48:33 -0300

    strings

--- AutoPublish/src/com/lea87crzz/AutoPublish/TestTareas.java
+++ AutoPublish/src/com/lea87crzz/AutoPublish/TestTareas.java
@@ -12,26 +12,32 @@ public class TestTareas {
 	public static void main(String[] args) {
 		Proceso proc=new Proceso();
 		
+		String srcDspace="C:\\Users\\lpujol\\git\\dspace6base\\";
+		String tomcatWebapps="C:\\usr\\local\\tomcat\\webapps\\";
+		
 		Proceso compilarApi=new Proceso("Compilar dspace-api");
-		compilarApi.agregarTarea(new MavenPackage("C:\\Users\\lpujol\\git\\dspace6base\\dspace-api"));
-		compilarApi.agregarTarea(new CopyFileTarea("C:\\Users\\lpujol\\git\\dspace6base\\dspace-api\\target\\",
+		compilarApi.agregarTarea(new MavenPackage(srcDspace+"dspace-api"));
+		compilarApi.agregarTarea(new CopyFileTarea(srcDspace+"dspace-api\\target\\",
 				"C:\\Users\\lpujol\\.m2\\repository\\org\\dspace\\dspace-api\\6.0\\",
 				"dspace-api-6.0.jar"));
 		
 		
 		Proceso compilarJSPUI=new Proceso("Compilar dspace-jspui");
-		compilarJSPUI.agregarTarea(new MavenPackage("C:\\Users\\lpujol\\git\\dspace6base\\dspace-jspui"));
-		compilarJSPUI.agregarTarea(new CopyDirTarea("C:\\Users\\lpujol\\git\\dspace6base\\dspace-jspui\\target\\dspace-jspui-6.0\\",
-				"C:\\usr\\local\\tomcat\\webapps\\admin\\"));
+		compilarJSPUI.agregarTarea(new MavenPackage(srcDspace+"dspace-jspui"));
+		compilarJSPUI.agregarTarea(new CopyDirTarea(srcDspace+"dspace-jspui\\target\\dspace-jspui-6.0\\",
+				tomcatWebapps+"admin\\"));
 		
+		Proceso compilarXMLUI=new Proceso("Compilar dspace-jspui");
+		compilarXMLUI.agregarTarea(new MavenPackage(srcDspace+"dspace-xmlui"));
+		compilarXMLUI.agregarTarea(new CopyDirTarea(srcDspace+"dspace-xmlui\\target\\dspace-xmlui-6.0\\",
+				tomcatWebapps+"ROOT\\"));
 		
 		Proceso levantarTomcat=new Proceso("Levantar tomcat");
 		levantarTomcat.agregarTarea(new CleanDirTarea("C:\\usr\\local\\dspace\\log\\"));
 		levantarTomcat.agregarTarea(new CleanDirTarea("C:\\usr\\local\\tomcat\\logs\\"));
 		levantarTomcat.agregarTarea(new ExcecuteCommand("C:\\usr\\local\\tomcat\\bin\\","startup.bat"));
 		
-		proc.agregarTarea(levantarTomcat);
-		
+		proc.agregarTarea(levantarTomcat);		
 		proc.ejecutar();
 
 	}
